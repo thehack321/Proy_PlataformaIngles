4eb8560e9010c383c1daf1b373ad84e56dde55fc
thehack321 <sabrinaal123>
2015-07-03 18:58:28 -0500

    Primer Envio

---
+++ Proy_EnglishPlatform/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Proy_EnglishPlatform.Startup))]
+namespace Proy_EnglishPlatform
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
